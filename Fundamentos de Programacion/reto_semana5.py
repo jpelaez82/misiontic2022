9aba98d3dd9e97b94df8c791d071d85e3090031e
Paul John Pelaez <jpelaez@unab.edu.co>
2020-10-23 20:21:07 -0500

    updating Reto Semana 5 a plotting

--- Fundamentos de Programacion/reto_semana5.py
+++ Fundamentos de Programacion/reto_semana5.py
@@ -1,77 +1,50 @@
 import pandas as pd
-import os 
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Validar Extension CSV del archivo de datos
-ruta_archivo_csv = "C:/misiontic2022/owid-covid-data.csv"
-ext = ruta_archivo_csv.split(".")
-myext = ext[-1]
+def caso_who(ruta_archivo_csv: str)-> dict:
 
-if myext == 'csv':
-    try:
-        # Crear dataframe
-        df = pd.read_csv('owid-covid-data.csv',                
-                    usecols=[
-                    "date",
-                    "continent",
-                    "total_cases_per_million",
-                    "population",
-                    "hospital_beds_per_thousand"
-                    ])
+    # Validar Extension CSV del archivo de datos
+    # ext = ruta_archivo_csv.split(".")
+    # myext = ext[-1]
 
-        # Fecha en formato DATETIME en Pandas
-        df["date"]= pd.to_datetime(df["date"]) 
+    if ruta_archivo_csv.endswith('.csv'):
+        try:
+            # Crear dataframe y seleccionar columnas a trabajar
+            df = pd.read_csv(ruta_archivo_csv,                
+                        usecols=[
+                        "date",
+                        "continent",
+                        "total_cases_per_million",
+                        "population",
+                        "hospital_beds_per_thousand"
+                        ])
+            
+            # Fecha en formato DATETIME para Pandas
+            df["date"] = pd.to_datetime(df["date"]) 
+            
+            #  Calculo del promedio de la razón entre el número total de casos de COVID-19 y el número total de camas de hospital disponibles
+            df['total_cases'] = df['total_cases_per_million'] * df['population'] / 1000000
+            df['total_beds'] = df['hospital_beds_per_thousand'] * df['population'] / 1000
+            df['rate'] = df['total_cases'] / df['total_beds']
+            
+            # Agrupar el dataframe por FECHA y CONTINENTE
+            group = df.groupby(['date', 'continent'])
+            # Asignar dataframe df_respuesta
+            df_respuesta = group['rate'].agg(np.mean).unstack()
+            
+            # Grafico del Dataframe 
+            df_respuesta.plot()
+            plt.show()
 
-        # Separacion en GRUPOS por CONTINENTE 
-        africa = df[df.continent == 'Africa']
-        africa.set_index('date', inplace=True)
-        africa['total_cases'] = africa['total_cases_per_million'] * africa['population'] / 1000000
-        africa['total_beds'] = africa['hospital_beds_per_thousand'] * africa['population'] / 1000
-        print(africa)
+            return df_respuesta.to_dict()
+        
+        except:
+            return "Error al leer el archivo de datos."
+        
 
-        europe = df[df.continent == 'Europe']
-        europe.set_index('date', inplace=True)
-        europe['total_cases'] = europe['total_cases_per_million'] * europe['population'] / 1000000
-        europe['total_beds'] = europe['hospital_beds_per_thousand'] * europe['population'] / 1000
-        print(europe)
-
-        asia = df[df.continent == 'Asia']
-        asia.set_index('date', inplace=True)
-        asia['total_cases'] = (asia['total_cases_per_million']/asia['population']) * 1000000
-        #print(asia)
-
-        north_america = df[df.continent == 'North America']
-        north_america.set_index('date', inplace=True)
-        north_america['total_cases'] = (north_america['total_cases_per_million']/north_america['population']) * 1000000
-        #print(north_america)
-
-        south_america = df[df.continent == 'South America']
-        south_america.set_index('date', inplace=True)
-        south_america['total_cases'] = (south_america['total_cases_per_million']/south_america['population']) * 1000000
-        #print(south_america)
-
-        # Calculo de TOTAL CASOS
-        # Formula: TotalCasos_porMillon / PoblacionTotal * 1.000.000
-
-
-        # dato = df.groupby('continent').total_cases_per_million.mean()
-
-        # Formato FECHA para tipo Pandas 
-        # dato = pd.to_datetime(df.date)
-
-        # Agrupar for Fechas y por Continentes
-        # print (df.groupby(['date','continent']).groups)
-
-
-        # Agrupar data series por CONTINENTE
-        # continentes = df.groupby('continent').total_cases_per_million
-        pass
-    except:
-        print("Error al leer el archivo de datos.")
-        pass    
-    
-
-else: 
-    print("Extensión inválida.")
+    else: 
+        return "Extensión inválida."
 
 
 
